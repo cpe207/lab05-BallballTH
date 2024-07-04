926fe37af7d9516707e8d93cd88939d3f282578c
BallChon <Buranin_b@cmu.ac.th>
2024-07-04 15:00:38 +0700

    finished

--- q2.ts
+++ q2.ts
@@ -1,9 +1,20 @@
 // define interface for Student object
+
+import { stderr } from "process";
+
 /* Your code here */
+interface student {
+  name:string;
+  score:number;
+}
 
 // assign interface/type to the function definition properly
-function findTopNames(students) {
+function findTopNames(students:student[]) {
   /* Your code here */
+  const passed = students
+  .filter((std) => std.score > 8)
+  .map((std) => std.name);
+  return passed;
 }
 
 // assign interface/type to the student1 object properly
